c8e6ac74e873db06e58cd4a0f856d5fd4a5f9a4f
ChuanXin <ngcx0103@gmail.com>
2023-05-17 15:54:03 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import coolendarLogo from './Coolendar logo.jpg';
 import './App.css';
 
 function App() {
+  const [user, setUserPair] = useState({});
+
+  const findPassword = (email) => {
+    return user[email];
+  };
+
+  function signUp() {
+    const email = document.getElementById('user-email');
+    const password = document.getElementById('user-password');
+
+    if (email !== '' && password !== '') {
+      const updatedUserPairs = { ...user, [email]: password };
+      setUserPair(updatedUserPairs);
+    } else {
+      const element = document.createElement('div');
+      element.innerText = 'Invalid input!';
+      reset(element);
+    }
+
+    console.log('sign up');
+
+    // Clear input fields after signing up
+    document.getElementById('user-email').value = '';
+    document.getElementById('user-password').value = '';  
+
+  }
+    
+  function logIn() {
+    const email = document.getElementById('user-email').value;
+    const password = document.getElementById('user-password').value;
+
+    const pwd = findPassword(email);
+
+    const element = document.createElement('div');
+
+    console.log('log in');
+
+    if (pwd !== undefined) {
+      if (pwd !== password) {
+        element.innerText = 'Wrong password!';
+      } else {
+        element.innerText = 'Successfully logged in!';
+      }
+    } else {
+      element.innerText = 'Invalid Email!';
+    }
+    
+    reset(element);
+  }
+
+  function reset(element) {
+    // Clear input fields after signing up
+    document.getElementById('user-email').value = '';
+    document.getElementById('user-password').value = ''; 
+    const user = document.getElementById('testing');
+    user.innerHTML = ''; // Clear existing content
+    user.appendChild(element);
+  }
+
+  
   return (
-    <div className="App">
+    <div className="Coolendar-App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={coolendarLogo} className="App-logo" alt="logo" />
+
+        
+        <div className="login-form">
+          <div className="title">Sign In</div>
+          
+          <div className="container">
+            <div className="enter">Email:</div>
+            <input
+            id="user-email"
+            type="text"
+            className="inputContainer"
+            placeholder="Enter your Email here"
+            />
+          </div>
+
+          <div className="container">
+            <div className="enter">Password:</div>
+            <input
+              id="user-password"
+              type="password"
+              className="inputContainer"
+              placeholder="Enter your password here"
+            />
+          </div>
+
+          <div className="buttonContainer">
+            <button className="sign-in-buttons" onClick={logIn}>Log In</button>
+          </div>
+
+          <div className="buttonContainer">
+            <button className="sign-in-buttons" onClick={signUp}>Sign Up</button>
+          </div>
+
+        </div>
+
+        <div id="testing"></div>
+
       </header>
     </div>
   );
