520a13de07606527f4bb394b9905e7182eee409c
ChuanXin <ngcx0103@gmail.com>
2023-05-19 14:56:57 +0800

    calendar v2

--- src/App.js
+++ src/App.js
@@ -4,146 +4,121 @@ import coolendarLogo from './Coolendar logo.jpg';
 import './App.css';
 
 function App() {
-  // const [user, setUserPair] = useState({});
-
-  // const findPassword = (email) => {
-  //   return user[email];
-  // };
-
+  const [user, setUserPair] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false); // Track login state
   const [date, setDate] = useState(new Date());
 
-  // function signUp() {
-  //   const email = document.getElementById('user-email');
-  //   const password = document.getElementById('user-password');
+  function signUp() {
+    const email = document.getElementById('user-email').value;
+    const password = document.getElementById('user-password').value;
 
-  //   if (email !== '' && password !== '') {
-  //     const updatedUserPairs = { ...user, [email]: password };
-  //     setUserPair(updatedUserPairs);
-  //   } else {
-  //     const element = document.createElement('div');
-  //     element.innerText = 'Invalid input!';
-  //     reset(element);
-  //   }
+    if (email !== '' && password !== '') {
+      const updatedUserPairs = { ...user, [email]: password };
+      setUserPair(updatedUserPairs);
+    } else {
+      const element = document.createElement('div');
+      element.innerText = 'Invalid input!';
+      reset(element);
+    }
 
-  //   console.log('sign up');
+    console.log('sign up');
 
-  //   // Clear input fields after signing up
-  //   document.getElementById('user-email').value = '';
-  //   document.getElementById('user-password').value = '';  
-
-  // }
+    // Clear input fields after signing up
+    document.getElementById('user-email').value = '';
+    document.getElementById('user-password').value = '';  
+  }
     
-  // function logIn() {
-  //   const email = document.getElementById('user-email').value;
-  //   const password = document.getElementById('user-password').value;
+  function logIn() {
+    const email = document.getElementById('user-email').value;
+    const password = document.getElementById('user-password').value;
 
-  //   const pwd = findPassword(email);
+    // Perform login verification
+    if (email !== '' && password !== '') {
+      const pwd = user[email];
 
-  //   const element = document.createElement('div');
+      if (pwd !== undefined) {
+        if (pwd !== password) {
+          alert('Wrong password!');
+        } else {
+          setLoggedIn(true); // Set login state to true
+        }
+      } else {
+        alert('Invalid email!');
+      }
+    } else {
+      alert('Please enter email and password!');
+    }
+  }
 
-  //   console.log('log in');
+  function reset(element) {
+    // Clear input fields after signing up
+    document.getElementById('user-email').value = '';
+    document.getElementById('user-password').value = ''; 
+    const user = document.getElementById('testing');
+    user.innerHTML = ''; // Clear existing content
+    user.appendChild(element);
+  }
 
-  //   if (pwd !== undefined) {
-  //     if (pwd !== password) {
-  //       element.innerText = 'Wrong password!';
-  //     } else {
-  //       element.innerText = 'Successfully logged in!';
-  //     }
-  //   } else {
-  //     element.innerText = 'Invalid Email!';
-  //   }
-    
-  //   reset(element);
-  // }
-
-  // function reset(element) {
-  //   // Clear input fields after signing up
-  //   document.getElementById('user-email').value = '';
-  //   document.getElementById('user-password').value = ''; 
-  //   const user = document.getElementById('testing');
-  //   user.innerHTML = ''; // Clear existing content
-  //   user.appendChild(element);
-  // }
-
+  if (loggedIn) {
     return (
       <div className="Coolendar-App">
         <div className="header">
           <img className="App-logo" src={coolendarLogo} alt="logo" />
-          <button className="year">2023</button>
         </div>
 
         <div>
-          {/* <div className="calendar-container">
-            <Calendar className="calendar" onChange={setDate} value={date}/>
+          <div className="calendar-container">
+            <Calendar className="calendar" onChange={setDate} value={date} selectRange={true} />
           </div>
           <div className="text-center">
-              Selected date: {date.toDateString()}
-          </div> */}
-
-<div>
-     <Calendar onChange={setDate} value={date} selectRange={true}/>
-   </div>
-   {date.length > 0 ? (
-   <p>
-     <span>Start:</span>{' '} {date[0].toDateString()}
-     &nbsp; to &nbsp;
-     <span>End:</span> {date[1].toDateString()}
-   </p>
-        ) : (
-   <p>
-     <span>Default selected date:</span>{' '} {date.toDateString()}
-   </p>
-        )}
- </div>
-
+            Selected date: {date.toDateString()}
+          </div>
         </div>
+      </div>
     );
+  }
 
-  
-  // return (
-  //   <div id="page" className="CoolendarLogin-App">
-  //     <header className="AppLogin-header">
-  //       <img src={coolendarLogo} className="AppLogin-logo" alt="logo" />
+  return (
+    <div className="CoolendarLogin-App">
+      <header className="AppLogin-header">
+        <img src={coolendarLogo} className="AppLogin-logo" alt="logo" />
 
-        
-  //       <div className="login-form">
-  //         <div className="login-title">Sign In</div>
+        <div className="login-form">
+          <div className="login-title">Sign In</div>
           
-  //         <div className="login-container">
-  //           <div className="enterLogin">Email:</div>
-  //           <input
-  //           id="user-email"
-  //           type="text"
-  //           className="login-inputContainer"
-  //           placeholder="Enter your Email here"
-  //           />
-  //         </div>
+          <div className="login-container">
+            <div className="enterLogin">Email:</div>
+            <input
+              id="user-email"
+              type="text"
+              className="login-inputContainer"
+              placeholder="Enter your Email here"
+            />
+          </div>
 
-  //         <div className="login-container">
-  //           <div className="enterLogin">Password:</div>
-  //           <input
-  //             id="user-password"
-  //             type="password"
-  //             className="login-inputContainer"
-  //             placeholder="Enter your password here"
-  //           />
-  //         </div>
+          <div className="login-container">
+            <div className="enterLogin">Password:</div>
+            <input
+              id="user-password"
+              type="password"
+              className="login-inputContainer"
+              placeholder="Enter your password here"
+            />
+          </div>
 
-  //         <div>
-  //           <button className="sign-in-buttons" onClick={logIn}>Log In</button>
-  //         </div>
+          <div>
+            <button className="sign-in-buttons" onClick={logIn}>Log In</button>
+          </div>
 
-  //         <div>
-  //           <button className="sign-in-buttons" onClick={signUp}>Sign Up</button>
-  //         </div>
+          <div>
+            <button className="sign-in-buttons" onClick={signUp}>Sign Up</button>
+          </div>
+        </div>
 
-  //       </div>
-
-  //       <div id="testing"></div>
-
-  //     </header>
-  //   </div>
-  // );
+        <div id="testing"></div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
