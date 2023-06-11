9af35913d0c54bf401cddab932c5915d512daa05
jinhanfromNUS <111408810+jinhanfromNUS@users.noreply.github.com>
2023-06-11 14:59:12 +0800

    Add files via upload

--- src/components/page/CoolendarScreen.js
+++ src/components/page/CoolendarScreen.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Calendar from 'react-calendar';
-import "../css/App.css";
 import { supabase } from '../../supabase';
+import Calendar from 'react-calendar';
+import coolendarLogo from "../images/Coolendar logo light cropped.png";
 import Navbar from "./Navbar";
-import Logo from "./Logo";
+import "../css/App.css";
 
 
 function calendarScreen({ token }) {
 
+  // navigation purposes
   let navigate = useNavigate();
 
   const [date, setDate] = useState(new Date());
@@ -17,12 +18,19 @@ function calendarScreen({ token }) {
 
   console.log(token);
 
+  function toUserScreen() {
+    navigate('/user');
+  }
+
   useEffect(() => {
     const fetchtodo = async () => {
       try {
+
+        const user_id = token.user.id;
         const { data, error } = await supabase
-        .from('todo')
-        .select('*')
+          .from('todotable')
+          .select()
+          .eq('creator_id', user_id);
 
         if (error) {
           throw error;
@@ -38,22 +46,25 @@ function calendarScreen({ token }) {
     fetchtodo();
   }, [])
 
-  function totestpage() {
-    navigate("/todotest");
+  function toTodoPage() {
+    navigate("/todo");
   }
 
   return (
     <div className="Coolendar-App">
-      <Logo/>
-      Welcome back, {token.user.user_metadata.name}
+      <div className="header">
+        <img className="App-logo" src={coolendarLogo} alt="logo" onClick={toUserScreen} />
+        Welcome back, {token.user.user_metadata.name}, {token.user.id}
+      </div>
+
       <div>
         <div className="calendar-container">
           <Calendar
             className="calendar"
             onChange={setDate}
             value={date}
-            // selectRange={true} 
-            // onClickDay={() => setTodoListVisible(true)}
+          // selectRange={true} 
+          // onClickDay={() => setTodoListVisible(true)}
           />
         </div>
         {date.length > 0 ? (
@@ -72,21 +83,23 @@ function calendarScreen({ token }) {
           {todo.map(t => (
             // eslint-disable-next-line react/jsx-key
             <div>
-              {t.user_id}, {t.todo_task}, {t.todo_date}
+              <div> Task: {t.todo_task} </div>
+              <div> Deadline: {t.deadline_date} </div>
+              <div> Finish by: {t.deadline_time != null ? t.deadline_time : 'Time is not set.'} </div>
             </div>
           ))}
         </div>
 
         {/* <Todo todoListVisible={todoListVisible} /> */}
 
-        <button onClick={totestpage}>try insert todo</button>
-
-        <React.Fragment>
-          <Navbar />
-        </React.Fragment>
+        <button onClick={toTodoPage}>Add new Todo Task</button>
 
       </div>
-    </div>
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    </div >
+
   );
 
 }
