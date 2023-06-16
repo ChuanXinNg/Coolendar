1fa7ff565b5a43733f574ed1a2e6bfe6f0a5a252
jinhanfromNUS <111408810+jinhanfromNUS@users.noreply.github.com>
2023-06-17 03:27:42 +0800

    Update EventPage.js

--- src/components/page/EventPage.js
+++ src/components/page/EventPage.js
@@ -296,7 +296,7 @@ function EventPage({ token }) {
         </form>
       ) : (
         <form className="form" onSubmit={handleEvent}>
-          <div className="title"> Write your day</div>
+          <div className="title"> Add new Event </div>
           <div>
             Name:{" "}
             <input type="text"
