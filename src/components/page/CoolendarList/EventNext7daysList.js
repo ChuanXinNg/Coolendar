bbeaa0db6dbb756e64262f3b4e7d51b87a0ed121
jinhanfromNUS <111408810+jinhanfromNUS@users.noreply.github.com>
2023-07-23 14:10:31 +0800

    Update EventNext7daysList.js
    Solve the bug where event date and event time is not updatable

--- src/components/page/CoolendarList/EventNext7daysList.js
+++ src/components/page/CoolendarList/EventNext7daysList.js
@@ -6,270 +6,284 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line react/prop-types
 function EventNext7daysList({ token, date }) {
 
-    const [editingEvent, setEditingEvent] = useState(null);
-    const [selectedEventContent, setSelectedEventContent] = useState('');
-    const [event, setEvent] = useState({
-      creator_id: "",
-      event_name: "",
-      event_info: "",
-      event_date: "",
-      event_time: ""
+  const [editingEvent, setEditingEvent] = useState(null);
+  const [selectedEventContent, setSelectedEventContent] = useState('');
+  const [event, setEvent] = useState({
+    creator_id: "",
+    event_name: "",
+    event_info: "",
+    event_date: "",
+    event_time: ""
+  });
+
+  // i have no idea what this is
+  EventNext7daysList.propTypes = {
+    token: PropTypes.shape({
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired
+      }).isRequired
+    }).isRequired
+  };
+
+  // read data from database
+  const [eventTable, setEventTable] = useState([]);
+  useEffect(() => {
+    const fetchEventTable = async () => {
+      try {
+
+        const user_id = token.user.id;
+        const currentDate = format(date, 'yyyy-MM-dd');
+        const seventhDay = addDays(date, 7);
+        const seventhDayFormatted = format(seventhDay, 'yyyy-MM-dd');
+        const { data, error } = await supabase
+          .from('eventtable')
+          .select()
+          .order('event_date', { ascending: true })
+          .gt('event_date', currentDate)
+          .lte('event_date', seventhDayFormatted)
+          .eq('creator_id', user_id);
+
+        if (error) {
+          throw error;
+        }
+
+        setEventTable(data);
+        console.log(data);
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchEventTable();
+  }, [date])
+
+  async function handleDeleteEvent(id) {
+    try {
+      const { data, error } = await supabase
+        .from('eventtable')
+        .delete()
+        .eq('id', id);
+
+      if (error) {
+        throw error;
+      }
+
+      console.log(data);
+      alert("This event will be deleted");
+
+      const updatedEvent = eventTable.filter(item => item.id !== id);
+      setEventTable(updatedEvent);
+      location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleEditEvent(e) {
+    setEditingEvent(e);
+    setEvent({
+      creator_id: e.creator_id,
+      event_name: e.event_name,
+      event_info: e.event_info,
+      event_date: e.event_date,
+      event_time: e.event_time
     });
+  }
 
-    // i have no idea what this is
-    EventNext7daysList.propTypes = {
-        token: PropTypes.shape({
-            user: PropTypes.shape({
-                id: PropTypes.string.isRequired
-            }).isRequired
-        }).isRequired
-    };
+  function handleEventChange(event) {
+    const { name, value } = event.target;
+    setEvent(prevFormData => ({
+      ...prevFormData,
+      creator_id: token.user.id,
+      [name]: value
+    }));
+  }
 
-    // read data from database
-    const [eventTable, setEventTable] = useState([]);
-    useEffect(() => {
-        const fetchEventTable = async () => {
-            try {
+  async function handleUpdateEvent(e) {
+    e.preventDefault();
+    if (event.event_name == "" && event.event_date == "" && event.event_time == "") {
+      alert("Please enter event name, event date and event time.");
+    } else if (event.event_name == "" && event.event_date == "") {
+      alert("Please enter event name and event date.");
+    } else if (event.event_name == "" && event.event_time == "") {
+      alert("Please enter event name and event time.");
+    } else if (event.event_date == "" && event.event_time == "") {
+      alert("Please enter event date and event time.");
+    } else if (event.event_name == "") {
+      alert("Please enter event name.");
+    } else if (event.event_date == "") {
+      alert("Please enter event date.");
+    } else if (event.event_time == "") {
+      alert("Please enter event time.");
+    } else {
+      try {
+        const { data, error } = await supabase
+          .from('eventtable')
+          .update({
+            event_name: event.event_name,
+            event_info: event.event_info,
+            event_date: event.event_date,
+            event_time: event.event_time
+          })
+          .eq('id', editingEvent.id);
 
-                const user_id = token.user.id;
-                const currentDate = format(date, 'yyyy-MM-dd');
-                const seventhDay = addDays(date, 7);
-                const seventhDayFormatted = format(seventhDay, 'yyyy-MM-dd');
-                const { data, error } = await supabase
-                    .from('eventtable')
-                    .select()
-                    .order('event_date', { ascending: true })
-                    .gt('event_date', currentDate)
-                    .lte('event_date', seventhDayFormatted)
-                    .eq('creator_id', user_id);
-
-                if (error) {
-                    throw error;
-                }
-
-                setEventTable(data);
-                console.log(data);
-
-            } catch (err) {
-                console.log(err);
-            }
+        if (error) {
+          throw error;
         }
-        fetchEventTable();
-    }, [date])
 
-    async function handleDeleteEvent(id) {
-        try {
-          const { data, error } = await supabase
-            .from('eventtable')
-            .delete()
-            .eq('id', id);
-    
-          if (error) {
-            throw error;
-          }
-    
-          console.log(data);
-          alert("This event will be deleted");
-    
-          const updatedEvent = eventTable.filter(item => item.id !== id);
-          setEventTable(updatedEvent);
-        } catch (err) {
-          console.log(err);
-        }
-      }
+        console.log(data);
+        location.reload();
 
-    function handleEditEvent(e) {
-        setEditingEvent(e);
+        setEditingEvent(null);
         setEvent({
-          creator_id: e.creator_id,
-          event_name: e.event_name,
-          event_info: e.event_info  ,
-          event_date: e.event_date,
-          event_time: e.event_time
+          creator_id: "",
+          event_name: "",
+          event_content: "",
+          event_date: "",
+          event_time: "",
         });
-    }      
-
-    function handleEventChange(event) {
-        const { name, value } = event.target;
-        setEvent(prevFormData => ({
-          ...prevFormData,
-          creator_id: token.user.id,
-          [name]: value
-        }));
+      } catch (err) {
+        console.log(err);
+      }
     }
+  }
 
-    async function handleUpdateEvent(e) {
-        e.preventDefault();
-        if (event.event_name == "") {
-          alert("Please enter event name");
-        } else {
-          try {
-            const { data, error } = await supabase
-              .from('eventtable')
-              .update({
-                event_name: event.event_name,
-                event_info: event.event_info
-              })
-              .eq('id', editingEvent.id);
-    
-            if (error) {
-              throw error;
-            }
-    
-            console.log(data);
-            location.reload();
-    
-            setEditingEvent(null);
-            setEvent({
-              creator_id: "",
-              event_name: "",
-              event_content: "",
-              event_date: "",
-              event_time: "",
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        }
+  async function checkEvent(event) {
+    const idToCheck = event.target.id;
+    console.log(event);
+
+    try {
+      const user_id = token.user.id;
+      const { data, error } = await supabase
+        .from('eventtable')
+        .select()
+        .eq('creator_id', user_id)
+        .eq('id', idToCheck);
+
+      if (error) {
+        throw error;
       }
 
-      async function checkEvent(event) {
-        const idToCheck = event.target.id;
-        console.log(event);
-    
-        try {
-          const user_id = token.user.id;
-          const { data, error } = await supabase
-            .from('eventtable')
-            .select()
-            .eq('creator_id', user_id)
-            .eq('id', idToCheck);
-    
-          if (error) {
-            throw error;
-          }
-    
-          if (data.length > 0) {
-            const { event_date, event_time, event_name, event_info } = data[0];
-            const formattedTime = formatTime(event_time);
-    
-            setSelectedEventContent(
-              <React.Fragment>
-                <div> {event_name}</div>
-                <div>{event_info}</div>
-                <div> {event_date}, {formattedTime} </div>
-              </React.Fragment>
-            );
-          } else {
-            setSelectedEventContent('');
-          }
-    
-    
-        } catch (err) {
-          console.log(err);
-        }
-      }
+      if (data.length > 0) {
+        const { event_date, event_time, event_name, event_info } = data[0];
+        const formattedTime = formatTime(event_time);
 
-    function handleCloseEvent() {
+        setSelectedEventContent(
+          <React.Fragment>
+            <div>Event Name: {event_name}</div>
+            <div> Date: {event_date}, {formattedTime} </div>
+            <div>{event_info}</div>
+          </React.Fragment>
+        );
+      } else {
         setSelectedEventContent('');
+      }
+
+
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    function formatTime(timeString) {
-        const [hours, minutes] = timeString.split(':');
-        let formattedHours = parseInt(hours, 10);
-        if (formattedHours >= 0 && formattedHours < 10) {
-            // morning before 10am
-            return `0${formattedHours}:${minutes}am`;
-        } else if (formattedHours >= 10 && formattedHours < 12) {
-            // morning after 10am and before afternoon 12pm
-            return `${formattedHours}:${minutes}pm`;
-        } else if (formattedHours == 12) {
-            // afternoon on 12pm
-            return `${formattedHours}:${minutes}pm`;
-        } else if (formattedHours > 12 && formattedHours < 22) {
-            // afternoon after 12pm and before 10pm
-            formattedHours = formattedHours - 12;
-            return `0${formattedHours}:${minutes}pm`;
-        } else if (formattedHours >= 22 && formattedHours < 24) {
-            // afternoon after 10pm
-            formattedHours = formattedHours - 12;
-            return `${formattedHours}:${minutes}pm`;
-        } else {
-            return 'Bad Timing. Invalid Timing input.'
-        }
+  function handleCloseEvent() {
+    setSelectedEventContent('');
+  }
+
+  function formatTime(timeString) {
+    const [hours, minutes] = timeString.split(':');
+    let formattedHours = parseInt(hours, 10);
+    if (formattedHours >= 0 && formattedHours < 10) {
+      // morning before 10am
+      return `0${formattedHours}:${minutes}am`;
+    } else if (formattedHours >= 10 && formattedHours < 12) {
+      // morning after 10am and before afternoon 12pm
+      return `${formattedHours}:${minutes}pm`;
+    } else if (formattedHours == 12) {
+      // afternoon on 12pm
+      return `${formattedHours}:${minutes}pm`;
+    } else if (formattedHours > 12 && formattedHours < 22) {
+      // afternoon after 12pm and before 10pm
+      formattedHours = formattedHours - 12;
+      return `0${formattedHours}:${minutes}pm`;
+    } else if (formattedHours >= 22 && formattedHours < 24) {
+      // afternoon after 10pm
+      formattedHours = formattedHours - 12;
+      return `${formattedHours}:${minutes}pm`;
+    } else {
+      return 'Bad Timing. Invalid Timing input.'
     }
+  }
 
-    return (
+  return (
 
-        <div className="eventTodayList">
-            {selectedEventContent && (
-            <div style={{border:"solid", borderWidth:"1px", padding:"5px"}}>
-              <b>Event info:</b>
-              <div>{selectedEventContent}</div>
-              <button
-                className="small-button"
-                style={{ marginLeft: '12px' }}
-                onClick={handleCloseEvent}
-              >Close</button>
-            </div>
-          )}
-            {editingEvent ? (
-          <form className="form" onSubmit={handleUpdateEvent}>
-            <div className="title"> Edit Event</div>
+    <div className="eventTodayList">
+      {editingEvent ? (
+        <form className="form" onSubmit={handleUpdateEvent}>
+          <div className="title"> Edit Event</div>
+          <div>
+            New Name:{" "}
+            <input type="text"
+              name="event_name"
+              value={event.event_name}
+              onChange={handleEventChange} />
+          </div>
+          <div>
+            New Info:{" "}
+            <input type="text"
+              name="event_info"
+              value={event.event_info}
+              onChange={handleEventChange} />
+          </div>
+          <div>
+            New Date:{" "}
+            <input type="date"
+              name="event_date"
+              value={event.event_date}
+              onChange={handleEventChange} />
+          </div>
+          <div>
+            New Time:{" "}
+            <input type="time"
+              name="event_time"
+              value={event.event_time}
+              onChange={handleEventChange} />
+          </div>
+          <button className="submit" type="submit">
+            Edit Event
+          </button>
+        </form>
+      ) : (
+        <div></div>
+      )}
+      {eventTable.length === 0 ? (
+        <div>There is no event for the next 7 days!</div>
+      ) : (
+        eventTable.map(x => (
+          <div key={x.id} style={{ margin: "5px", borderBottom: "solid", borderBottomWidth: "1px" }}>
+            <div style={{ fontSize: "18px" }}> {x.event_name} </div>
+            <div> Date: {x.event_date} </div>
+            <div> Time: {formatTime(x.event_time)} </div>
+            <button className="small-button" id={x.id} onClick={checkEvent}> Check </button>
+            <button className="small-button" onClick={() => handleDeleteEvent(x.id)}>Delete</button>
+            <button className="small-button" onClick={() => handleEditEvent(x)}>Edit</button>
             <div>
-              New Name:{" "}
-              <input type="text"
-                name="event_name"
-                value={event.event_name}
-                onChange={handleEventChange} />
+              {selectedEventContent && (
+                <div>
+                  <b>Event info:</b>
+                  <div>{selectedEventContent}</div>
+                  <button
+                    className="small-button"
+                    style={{ marginLeft: '12px' }}
+                    onClick={handleCloseEvent}
+                  >Close</button>
+                </div>
+              )}
             </div>
-            <div>
-              New Info:{" "}
-              <input type="text"
-                name="event_info"
-                value={event.event_info}
-                onChange={handleEventChange} />
-            </div>
-            <div>
-              New Date:{" "}
-              <input type="date"
-                name="event_date"
-                value={event.event_date}
-                onChange={handleEventChange} />
-            </div>
-            <div>
-              New Time:{" "}
-              <input type="time"
-                name="event_time"
-                value={event.event_time}
-                onChange={handleEventChange} />
-            </div>
-            <button className="submit" type="submit">
-              Edit Event
-            </button>
-          </form>
-        ) : (
-          <div></div>
-        )}
-            {eventTable.length === 0 ? (
-                <div>There is no event for the next 7 days!</div>
-            ) : (
-                eventTable.map(x => (
-                    <div key={x.id} style={{margin: "5px", borderBottom:"solid", borderBottomWidth:"1px"}}>
-                        <div style={{fontSize: "18px", fontWeight: "bold"}}> {x.event_name} </div>
-                        <div> Date: {x.event_date} </div>
-                        <div> Time: {formatTime(x.event_time)} </div>
-                        <button className="small-button" id={x.id} onClick={checkEvent}> Check </button>
-                        <button className="small-button" onClick={() => handleDeleteEvent(x.id)}>Delete</button>
-                        <button className="small-button" onClick={() => handleEditEvent(x)}>Edit</button>
-                        <div>
-        </div>
-                    </div>
-                ))
-            )}
-            
-        </div>
-    );
+          </div>
+        ))
+      )}
+    </div>
+  );
 
 }
 
